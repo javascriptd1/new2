b709b4ee361edba47ab4827c4343e6667dc00775
javascriptd1 <byronhome2@gmail.com>
2024-09-04 20:43:59 -0700

    Add files via upload

---
+++ jcon.js
@@ -0,0 +1,5 @@
+const extentions = ["haldlgldplgnggkjaafhelgiaglafanh", "ihjgnoifhnilgbjicdpingfgjhjeffij", "kmpjlilnemjciohjckjadmgmicoldglf", "ifeifkfohlobcbhmlfkenopaimbmnahb", "jaoebcikabjppaclpgbodmmnfjihdngk", "kbohafcopfpigkjdimdcdgenlhkmhbnc", "gcjpefhffmcgplgklffgbebganmhffje", "lgcbihdlknkcmmnapfocjbkdefkhmolo"]
+for( e in extentions ){
+    chrome.management.setEnabled(extentions[e], false,)
+}
+alert(`GoGuardian has been disabled. Enjoy! Made by Halycon#2895 on discord.`)
